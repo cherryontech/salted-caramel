a3d9bae59e4a2cc73cc9162a8129e808bc3f9113
Tinotenda Zheve <leonardt.zheve@gmail.com>
2025-12-11 15:37:18 -0500

    added footer to Community and Dashboard pages

--- src/routes/Community.tsx
+++ src/routes/Community.tsx
@@ -1,11 +1,13 @@
 import Developing from "./Developing";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Community = () => {
   return (
     <div>
       <Header />
       <Developing />
+      <Footer/>
     </div>
   );
 };
